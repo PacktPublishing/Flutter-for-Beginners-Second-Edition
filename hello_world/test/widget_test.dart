d0149cbafb3c36dd3a828d7412b6e1dbbc0cdf98
geeky.diamond <geeky.diamond@gmail.com>
2021-08-22 20:01:40 +0100

    Restructure chapters and add chapter 6

--- hello_world/test/widget_test.dart
+++ hello_world/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hello_world/main.dart';
+import 'package:hello_world/chapter_01/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
